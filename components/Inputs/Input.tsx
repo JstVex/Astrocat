45326d00b33fbe3c45a068544786c795aebf5161
JstVex <heinhtetlulu14@gmail.com>
2023-05-15 20:23:34 +0630

    Create reusable form input

---
+++ components/Inputs/Input.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import React from "react"
+import clsx from "clsx"
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form"
+
+interface InputProps {
+    label: string,
+    id: string,
+    type?: string,
+    required?: boolean,
+    register: UseFormRegister<FieldValues>,
+    errors: FieldErrors,
+    disabled?: boolean
+}
+
+export const Input: React.FC<InputProps> = ({ label, id, type, required, register, errors, disabled }) => {
+    return (
+        <div>
+            <label htmlFor={id} className="block text-sm font-medium leading-6 text-zinc-900">
+                {label}
+            </label>
+            <div className="mt-1">
+                <input
+                    id={id}
+                    type={type}
+                    autoComplete={id}
+                    disabled={disabled}
+                    {...register(id, { required })}
+                    className={clsx("form-input block w-full my-1.5 rounded-md border-0 py-1.5 text-zinc-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-zinc-400 focus:ring-1 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6", errors[id] && "focus:ring-rose-500", disabled && "opacity-50 cursor-default")}
+                />
+            </div>
+        </div>
+    )
+}
