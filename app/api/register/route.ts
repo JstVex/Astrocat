f60cdea0259fed0e67302c9f3f8f7279055b40fb
JstVex <heinhtetlulu14@gmail.com>
2023-05-16 00:51:56 +0630

    Create register route for registering new users

---
+++ app/api/register/route.ts
@@ -0,0 +1,35 @@
+import bcrypt from 'bcrypt';
+import prisma from '@/libs/prismadb';
+import { NextResponse } from 'next/server';
+
+export async function POST(
+    request: NextResponse
+) {
+    try {
+        const body = await request.json();
+        const {
+            name,
+            email,
+            password
+        } = body;
+
+        if (!name || !email || !password) {
+            return new NextResponse('Missing credentials', { status: 400 });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+                hashedPassword
+            }
+        });
+
+        return NextResponse.json(user);
+    } catch (error: any) {
+        console.log('REGISTERATION ERROR:', error);
+        return new NextResponse('Internal Error', { status: 500 });
+    }
+}
