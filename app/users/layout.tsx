cd3443f220139938bb70e4bfc497524863c62c1e
JstVex <heinhtetlulu14@gmail.com>
2023-05-18 00:13:01 +0630

    Create Sidebar with both desktop and mobile view

---
+++ app/users/layout.tsx
@@ -0,0 +1,16 @@
+import Sidebar from "@/components/sidebar/Sidebar"
+
+export default async function Userslayout({
+    children
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        // @ts-expect-error Server Component
+        <Sidebar>
+            <div className="h-full">
+                {children}
+            </div>
+        </Sidebar>
+    )
+}
