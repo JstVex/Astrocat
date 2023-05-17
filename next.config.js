c47115b5d2632cd4657b1fa69606c71c985c3771
JstVex <heinhtetlulu14@gmail.com>
2023-05-18 00:28:31 +0630

    Configure images' domains

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    swcPlugins: [
+        ["next-superjson-plugin", {}]
+    ],
+    images: {
+        domains: [
+            "res.cloudinary.com",
+            "avatars.githubusercontent.com",
+            "lh3.googleusercontent.com"
+        ]
+    }
+}
 
 module.exports = nextConfig
