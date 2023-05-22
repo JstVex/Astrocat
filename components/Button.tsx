29d86f33657f86b9229804ef25f4b322b8180afc
JstVex <heinhtetlulu14@gmail.com>
2023-05-22 23:53:39 +0630

    Add optional parameter for button type

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,7 +3,7 @@
 import clsx from "clsx"
 
 interface ButtonProps {
-    type: string,
+    type?: string,
     children?: React.ReactNode,
     onClick?: () => void,
     disabled?: boolean,
@@ -17,7 +17,7 @@ export const Button: React.FC<ButtonProps> = ({ type, children, onClick, disable
         <button
             onClick={onClick}
             disabled={disabled}
-            className={clsx("flex justify-center rounded-md mt-6 px-3 py-2 text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2", disabled && "opacity-50 cursor-default", fullWidth && "w-full", secondary ? "text-zinc-900" : "text-white", danger && "bg-rose-500 hover:bg-roe-600 focus-visible:outline-rose-600", !secondary && !danger && "bg-sky-500 hover:bg-sky-600 focus-visible:outline-sky-600")}
+            className={clsx("flex justify-center rounded-md px-3 py-2 text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2", disabled && "opacity-50 cursor-default", fullWidth && "w-full", secondary ? "text-zinc-900" : "text-white", danger && "bg-rose-500 hover:bg-roe-600 focus-visible:outline-rose-600", !secondary && !danger && "bg-sky-500 hover:bg-sky-600 focus-visible:outline-sky-600")}
         >
             {children}
         </button>
