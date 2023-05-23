3bbf4cf0ed21f0543493dc400f3b816aca5780f0
JstVex <heinhtetlulu14@gmail.com>
2023-05-23 20:03:37 +0630

    Add route for ai cat chatbot

--- hooks/useRoutes.ts
+++ hooks/useRoutes.ts
@@ -2,6 +2,7 @@ import { useMemo } from 'react';
 import { usePathname } from 'next/navigation';
 import { HiChat } from 'react-icons/hi';
 import { HiArrowLeftOnRectangle, HiUsers } from 'react-icons/hi2';
+import { IoLogoOctocat } from 'react-icons/io5'
 import { signOut } from 'next-auth/react';
 
 
@@ -23,6 +24,12 @@ const useRoutes = () => {
             href: '/users',
             icon: HiUsers,
             active: pathname === '/users'
+        },
+        {
+            label: 'Astrocat',
+            href: '/astrocat',
+            icon: IoLogoOctocat,
+            active: pathname === '/astrocats'
         },
         {
             label: 'Logout',
