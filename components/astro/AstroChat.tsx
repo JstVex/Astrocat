b9b703188aa821e4d7053335f46dae1733872453
JstVex <heinhtetlulu14@gmail.com>
2023-05-26 01:55:07 +0630

    Get messages between astro and current user and send it as prop to body

--- components/astro/AstroChat.tsx
+++ components/astro/AstroChat.tsx
@@ -1,13 +1,16 @@
 import AstroBody from "./AstroBody";
 import AstroForm from "./AstroForm";
 import AstroHeader from "./AstroHeader";
+import getAstroMessages from "@/actions/getAstroMessages";
 
-const AstroChat = () => {
+const AstroChat = async () => {
+    const messages = await getAstroMessages();
+
     return (
         <div className="h-full bg-zinc-100 dark:bg-zinc-900">
             <div className="h-full flex flex-col">
                 <AstroHeader />
-                <AstroBody />
+                <AstroBody messages={messages} />
                 <AstroForm />
             </div>
         </div>
