2afdc77546b7ec18b52f89a04bc6edd42dd15f3d
JstVex <heinhtetlulu14@gmail.com>
2023-05-26 01:52:30 +0630

    Display messages for astro

--- components/astro/AstroBody.tsx
+++ components/astro/AstroBody.tsx
@@ -1,8 +1,27 @@
-const AstroBody = () => {
+'use client'
+
+import { Astro, User } from "@prisma/client";
+import AstroMessageBox from "./AstroMessageBox";
+
+interface AstroBodyProps {
+    messages: (Astro & {
+        sender: User
+    })[]
+}
+
+const AstroBody: React.FC<AstroBodyProps> = ({ messages = [] }) => {
+
     return (
         <div className="flex-1 overflow-y-auto">
             <h3 className="text-zinc-100">
-                body
+                {messages.map((message) => {
+                    return (
+                        <AstroMessageBox
+                            key={message.id}
+                            data={message}
+                        />
+                    )
+                })}
             </h3>
         </div>
     );
