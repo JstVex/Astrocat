ea7cb0925b97aee90616565f989c96ca84b2ecd6
JstVex <heinhtetlulu14@gmail.com>
2023-05-25 15:14:28 +0630

    Remove the 'astro/new' route

--- app/astro/new/page.tsx
+++
@@ -1,9 +0,0 @@
-import AstroChat from "@/components/astro/AstroChat";
-
-export default function NewAstro() {
-    return (
-        <div className="h-full w-full md:pl-80">
-            <AstroChat />
-        </div>
-    )
-}
--- app/astro/page.tsx
+++ app/astro/page.tsx
@@ -1,9 +1,11 @@
+import AstroChat from "@/components/astro/AstroChat";
 import AstroState from "@/components/astro/AstroState";
 
 export default function AstroCat() {
     return (
         <div className="h-full  md:block md:pl-80">
-            <AstroState />
+            {/* <AstroState /> */}
+            <AstroChat />
         </div>
     )
 }
