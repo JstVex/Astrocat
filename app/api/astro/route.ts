b919f982fb9fc042c63333bf16a130abe512fc09
JstVex <heinhtetlulu14@gmail.com>
2023-05-26 02:01:44 +0630

    Create documents for sender and astro's reply for every message sent

--- app/api/astro/route.ts
+++ app/api/astro/route.ts
@@ -1,12 +1,19 @@
 import { NextResponse } from 'next/server';
 import openai from '@/utils/openai';
+import prisma from '@/libs/prismadb';
+import getCurrentUser from "@/actions/getCurrentUser";
 
 export async function POST(
     request: Request
 ) {
     try {
+        const currentUser = await getCurrentUser();
         const body = await request.json();
         const { message } = body;
+
+        if (!currentUser?.id || !currentUser?.email) {
+            return new NextResponse('Unauthorized', { status: 401 });
+        }
 
         const completion = await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
@@ -18,7 +25,35 @@ export async function POST(
         });
         const reply = completion.data.choices[0].message?.content;
 
-        return NextResponse.json(reply);
+        const senderMessage = await prisma.astro.create({
+            data: {
+                messageBySender: message,
+                sender: {
+                    connect: {
+                        id: currentUser.id,
+                    }
+                },
+            },
+            include: {
+                sender: true
+            }
+        })
+
+        const replyMessage = await prisma.astro.create({
+            data: {
+                messageByAstro: reply,
+                sender: {
+                    connect: {
+                        id: currentUser.id,
+                    }
+                },
+            },
+            include: {
+                sender: true
+            }
+        })
+
+        return NextResponse.json(replyMessage);
     } catch (error: any) {
         return new NextResponse(`Internal Error: ${error}`, { status: 500 })
     }
