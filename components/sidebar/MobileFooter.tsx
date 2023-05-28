cb20dec9b619c763471f6538f138c67fcc46c774
JstVex <heinhtetlulu14@gmail.com>
2023-05-28 16:51:39 +0630

    Add profile to mobile footer

--- components/sidebar/MobileFooter.tsx
+++ components/sidebar/MobileFooter.tsx
@@ -4,10 +4,19 @@ import useConversation from "@/hooks/useConversation";
 import useRoutes from "@/hooks/useRoutes";
 import MobileItem from "./MobileItem";
 import { usePathname } from "next/navigation";
+import Avatar from "../Avatar";
+import { User } from "@prisma/client";
+import { useState } from "react";
+import SettingsModal from "./SettingsModal";
 
-const MobileFooter = () => {
+interface MobileFooterProps {
+    currentUser: User
+}
+
+const MobileFooter: React.FC<MobileFooterProps> = ({ currentUser }) => {
     const routes = useRoutes();
     const { isOpen } = useConversation();
+    const [modalOpen, setModalOpen] = useState(false);
 
     const pathname = usePathname();
 
@@ -20,8 +29,14 @@ const MobileFooter = () => {
     }
 
     return (
-        <div
-            className="
+        <>
+            <SettingsModal
+                isOpen={modalOpen}
+                currentUser={currentUser}
+                onClose={() => setModalOpen(false)}
+            />
+            <div
+                className="
                 fixed 
                 justify-between 
                 w-full 
@@ -35,17 +50,34 @@ const MobileFooter = () => {
                 dark:bg-black
                 dark:border-zinc-700
             "
-        >
-            {routes.map((route) => (
-                <MobileItem
-                    key={route.href}
-                    href={route.href}
-                    active={route.active}
-                    icon={route.icon}
-                    onClick={route.onClick}
-                />
-            ))}
-        </div>
+            >
+                {routes.map((route) => (
+                    <MobileItem
+                        key={route.href}
+                        href={route.href}
+                        active={route.active}
+                        icon={route.icon}
+                    // onClick={route.onClick}
+                    />
+                ))}
+                <div onClick={() => setModalOpen(true)} className="
+                py-2
+                px-6
+                border-x-[0.5px]
+                border-zinc-300
+                transition
+                duration-300
+              text-gray-500 
+                hover:bg-gray-100
+                dark:hover:bg-zinc-800
+                dark:hover:text-zinc-200
+                dark:border-zinc-700"
+                >
+                    <Avatar user={currentUser} />
+                </div>
+
+            </div>
+        </>
     );
 }
 
