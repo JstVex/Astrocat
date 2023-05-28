2597bd77dd0c97c84a61fb455bee6298ba5a399e
JstVex <heinhtetlulu14@gmail.com>
2023-05-28 16:59:09 +0630

    Add align for settings modal

--- components/Inputs/Input.tsx
+++ components/Inputs/Input.tsx
@@ -11,10 +11,11 @@ interface InputProps {
     required?: boolean,
     register: UseFormRegister<FieldValues>,
     errors: FieldErrors,
-    disabled?: boolean
+    disabled?: boolean,
+    align?: boolean
 }
 
-export const Input: React.FC<InputProps> = ({ label, id, type, required, register, errors, disabled }) => {
+export const Input: React.FC<InputProps> = ({ label, id, type, required, register, errors, disabled, align }) => {
     return (
         <div>
             <label htmlFor={id} className="block text-sm font-medium leading-6 text-zinc-900 dark:text-zinc-50">
@@ -27,7 +28,7 @@ export const Input: React.FC<InputProps> = ({ label, id, type, required, registe
                     autoComplete={id}
                     disabled={disabled}
                     {...register(id, { required })}
-                    className={clsx("form-input block w-full my-1.5 rounded-md border-0 py-1.5 text-zinc-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-zinc-400 focus:ring-1 focus:ring-inset focus:ring-violet-500 sm:text-sm sm:leading-6 dark:bg-zinc-800/50 dark:ring-zinc-600 dark:text-zinc-50 dark:placeholder:text-zinc-200", errors[id] && "focus:ring-rose-500", disabled && "opacity-50 cursor-default")}
+                    className={clsx("form-input block w-full my-1.5 rounded-md border-0 py-1.5 text-zinc-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-zinc-400 focus:ring-1 focus:ring-inset focus:ring-violet-500 sm:text-sm sm:leading-6 dark:bg-zinc-800/50 dark:ring-zinc-600 dark:text-zinc-50 dark:placeholder:text-zinc-200", errors[id] && "focus:ring-rose-500", disabled && "opacity-50 cursor-default", align && "mt-[28px]")}
                 />
             </div>
         </div>
